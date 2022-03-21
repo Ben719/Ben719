f3baa032517d76761f1bb297b02285732362d95f
Ben719 <100649301+Ben719@users.noreply.github.com>
2022-03-21 13:14:18 -0700

    y

---
+++ Processing/new_part_for_project/DnaWork project.sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1200, 800);
+  background("Black");
+}
+function draw() {
+  fill("Red"); 
+  translate(mouseX, mouseY);
+  scale(mouseX / 200.0);
+  rect(-15, -15, 20, 0);
+  rect(130, 210, 50, 14);
+  stroke(255); // Set line drawing color to white
+  frameRate(30) 
+  
+   }
+ 
