841adad2ba780283f65a5633dfeb4b6719dd77b2
Ben719 <100649301+Ben719@users.noreply.github.com>
2022-04-27 16:01:44 -0700

    t

---
+++ game1/game1.js
@@ -0,0 +1,56 @@
+var ballx = 300;
+var bally = 300;
+var ballSize = 40;
+var score = 0;
+
+function setup() {
+  createCanvas(600, 600);
+  textAlign(CENTER);
+  textSize(20);
+} // end of setup ===============
+
+function draw() {
+  background(220);
+  
+  levelOne();
+  text(("Score: " + score),width/2,40);
+} // end of draw =================
+
+function levelOne(){
+  text("level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if (distToBall < ballSize/2){
+    ballx = random(width);
+    ballx = random(height);
+    score = score + 1;
+  }
+  
+  if(score>= 5){
+   ///background(random(255));
+  }
+  
+  line(ballx, bally, mouseX, mouseY);
+  ellipse(ballx,bally, ballSize, ballSize);
+  
+  
+} // end of level 1  ===============
+
+
+function levelTwo(){
+  text("level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if (distToBall < ballSize/2){
+    ballx = random(width);
+    ballx = random(height);
+    score = score + 1;
+  }
+  
+  if(score>= 10){
+   ///background(random(255));
+  }
+  
+  line(ballx, bally, mouseX, mouseY);
+  ellipse(ballx,bally, ballSize, ballSize);
+  
+  
+} // end of level 2  ===============
