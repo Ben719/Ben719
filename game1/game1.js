230010656f0f0757b8894d86c7e3de9dfff37d9e
Ben719 <100649301+Ben719@users.noreply.github.com>
2022-05-02 13:32:37 -0700

    y

---
+++ game1/game1.js
@@ -0,0 +1,66 @@
+var ballx = 300;
+var bally = 300;
+var ballSize = 40;
+var score = 0;
+var img1;
+
+function preload() {
+  
+  img1 = loadimage('https://ben719.github.io/game1/Spacebattle.jpg'); //background LI
+  
+}
+function setup() {
+  createCanvas(600, 600);
+  let c =color('red'); //text color
+  fill(c);
+  textAlign(CENTER);
+  textSize(20);
+} // end of setup ===============
+
+function draw() {
+  background(img1);
+  
+  
+  
+  levelOne();
+  text(("Score: " + score),width/2,40);
+} // end of draw =================
+
+function levelOne(){
+  text("level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if (distToBall < ballSize/2){
+    ballx = random(width);
+    ballx = random(height);
+    score = score + 1;
+  }
+  
+  if(score>= 5){
+   ///background(random(255));
+  }
+  
+  line(ballx, bally, mouseX, mouseY);
+  ellipse(ballx,bally, ballSize, ballSize);
+  
+  
+} // end of level 1  ===============
+
+
+function levelTwo(){
+  text("level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if (distToBall < ballSize/2){
+    ballx = random(width);
+    ballx = random(height);
+    score = score + 1;
+  }
+  
+  if(score>= 10){
+   ///background(random(255));
+  }
+  
+  line(ballx, bally, mouseX, mouseY);
+  ellipse(ballx,bally, ballSize, ballSize);
+  
+  
+} // end of level 2  ===============
